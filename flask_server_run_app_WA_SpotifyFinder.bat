ea297ab3aa8def15d33a598aca23faa2178745c1
Louie <lfg3141@gmail.com>
2021-12-04 08:28:43 -0500

    - bat file to run spotifyfinder server on a local win10 box for testing

---
+++ flask_server_run_app_WA_SpotifyFinder.bat
@@ -0,0 +1,17 @@
+
+@ECHO OFF
+
+REM Runs py script
+
+REM Start browser
+ECHO ---------Starting Browser-----------
+start "" http://127.0.0.1:5000
+
+REM Start browser
+ECHO ---------Starting Flask server app.py in WA_SpotifyFinder -----------
+ECHO -- waSpotifyFinderApp.py print statements follow --
+C:\Users\lfg70\.aa\LFG_Code\Python\WA_SpotifyFinder\venv\Scripts\python C:\Users\lfg70\.aa\LFG_Code\Python\WA_SpotifyFinder\waSpotifyFinderApp.py
+
+ECHO flask server app.py was started
+
+PAUSE
